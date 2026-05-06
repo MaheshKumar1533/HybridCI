3b36cce86250e9592ddee8d5c7ae46bf83f001e1
Mahesh Kumar <maheshkumarvmk0@gmail.com>
2026-05-07 00:35:22 +0530

    chore: remove legacy demonstration and setup scripts and reset database file

--- sample_repo/src/auth.py
+++
@@ -1,7 +0,0 @@
-def login(username,password):
-    if not username or not password:
-        return False
-    return username == "admin" and password == "admin123"
-
-def logout(user):
-    return True
--- sample_repo/src/utils.py
+++
@@ -1,6 +0,0 @@
-def is_even(n):
-    print("Checking if {} is even".format(n))
-    return n % 2 == 0
-
-def to_upper(text) :
-    return text.upper()
